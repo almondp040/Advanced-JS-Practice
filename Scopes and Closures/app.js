e4d5f2007d2db233f9cf33383901db87812909ee
almondp040 <almondpaschal@gmail.com>
2025-05-12 18:16:49 -0400

    More Ch.9 Practice with Closures

--- Scopes and Closures/app.js
+++ Scopes and Closures/app.js
@@ -15,3 +15,138 @@ if (true) {
 }
 
 //We will not see animal outside of the condition since its scoped to it
+
+
+//Scope Chain: 
+
+//JS looks for variables in this level: 
+//1. Local Scope, then 2. Outer Functions, Then 3. Global Scope
+let age = 10; //Global
+console.log(age); 
+
+function outer() {
+    //Now this will print out ageless due to the scope chain: 
+    let age = "ageless"; //Outer
+    console.log(age);
+
+    function inner() {
+        let age = "Eternal"; 
+        console.log(age); 
+
+         function superInner(params) {
+        console.log(age); //will print out Eternal since there's no age assigned in the superInner Function
+    }
+
+    superInner(); 
+    }
+    inner(); 
+
+
+}; 
+
+outer(); //If we do not run this function then JS will grab the global scope
+
+//Static Scoping 
+//Scope is determined when the variable is declared
+
+let animal = "Barn Owl"; 
+
+function printAnimal(params) {
+    console.log(animal); 
+}; 
+
+function alsoPrintAnimal(params) {
+    let animal = "Burrowing Owl"; 
+    console.log(animal); 
+}; 
+
+
+
+//Hoisting
+
+//Example of Hoisting when we just had var: 
+//How hoisting looks behind the scenes: 
+var food = undefined
+console.log(food); // this will be undefined since it knows what food is but it does not have a value yet
+food = "pizza";
+console.log(food);  
+
+
+//Immediately Involked Functional Expressions: Basically running a function ASAP!
+(
+    function () {
+        console.log("Immediately Involked Function!!!"); 
+    }
+)()
+
+//Closure Example: 
+//Inner functions remember the variables in the parent function
+function outerFunction() {
+    let outer = "Live from the Outer Function, JS will remember me!"; 
+
+    function innerFunction() {
+        console.log("From the inner function:", outer); 
+    }
+
+    //Spits out the inner function: 
+    return innerFunction; 
+}
+
+const myClosure = outerFunction(); 
+
+//ID Generator Closure example: 
+
+function idGenerate() {
+    let count = 1; 
+
+  return function goGenerate() {
+        console.log(count); 
+        return count++; 
+    }
+}
+
+const generateID = idGenerate(); 
+
+generateID(); 
+
+
+//Another Closure Example: 
+//Create a function that will return an object with a increment, decrement and getCount object
+//Basically setting variables in a parent function to be used in the child functions: 
+function createCounter() {
+    let count = 0; 
+
+    //Object to contain methods to count: 
+    const countObj = {
+        increment(){
+            count++; 
+            console.log("Increment",count); 
+            return count; 
+        }, 
+        decrement(){
+            count--; 
+            console.log("Decrement",count); 
+            return count; 
+        }, 
+        getCount(){
+            console.log("Current Count", count)
+        }
+
+    }
+
+    return countObj; 
+}
+
+const counter= createCounter(); 
+
+counter.getCount(); 
+counter.increment(); 
+
+function main() {
+    printAnimal(); 
+    alsoPrintAnimal(); 
+    myClosure(); 
+    counter.increment()
+}
+
+main(); 
--- Scopes and Closures/notes.md
+++ Scopes and Closures/notes.md
@@ -12,3 +12,36 @@
         - Same with const. 
     - These are blocked scoped so let and const will be scoped to these blocks
 
+- The Scope Chain
+    - When we use a variable in a line of code, it will conduct the search in this order: 
+        - In the Local Scope
+        - Any Outer Functions
+        - Global Scope
+    
+- Static Scope
+    - JS uses Static Scoping 
+    - The scope of a variable in JS is determined when it is declared
+    - Most languages are all static scoped and this makes it easier since the variables will not change when stuff is called!
+
+- Hoisting
+    - variable declarations are pulled to the top of their function
+        - So the variable is legal, but equal to undefined until definition
+        - Basically JS sets the value as undefined first, then sets the value of the variable 
+        - JS pulls the variable to the top of the scope, basically pulls variables to the top of either the file or the function, but this ONLY happens with var
+        - JS technically hoists const and let variables but it does not allow us to access these variables, this is called the Temporal Dead Zone
+        - Essentially use let and const and we will not encounter Hoisting issues
+
+- IIFE
+    - Immediately Involked Functional Expressions 
+    - Running a function without a name!
+    - Immediately running a function
+    - This is good for privacy, and will only run once
+    - Can also be good for global scope pollution, to limit things being in the global scope. 
+
+- Closures: The Basics
+    - The ability for inner functions to remember variables defined in outer functions, long after the outer function has returned
+    - allows the inner function to access stuff from the parent function once its ran
+    - Basically allows us to avoid using a bunch of global variables
+    - Allows us to create essentially private variables!
+    - Forces people to actually use the function, and does not allow us to access the count variable, useful to protect variables
+
