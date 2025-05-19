2356f9a771398310340ed22f0e6193b1fbfb718c
almondp040 <almondpaschal@gmail.com>
2025-05-19 09:51:32 -0400

    Ch.10 Practice

---
+++ Ch.10 Timers/app.js
@@ -0,0 +1,78 @@
+//Timers! Build in Browser APIs for JS 
+
+// setTimeout: 
+const myTimeOut = setTimeout(() => {
+    console.log("This will be ran every 2 seconds!"); 
+}, 2000);
+
+// An example of this would be a show message function
+const showNotification = (message, timer) =>{
+    const notification = document.createElement("div"); 
+    notification.innerText = message; 
+    document.body.appendChild(notification); 
+    //Remove the notification after a certain amount of time. 
+    const myTimeout =  setTimeout(() => { 
+        notification.remove(); 
+    }, timer);
+    clearTimeout(myTimeout); 
+}; 
+
+
+showNotification("Hello There!", 4000); 
+//showNotification("Signed In!", 5000); 
+//showNotification("Love Ya!", 10000); 
+
+
+//setInterval: 
+const myInterval = setInterval(() => {
+    console.log("This is my setInterval Function! This will run every 2 seconds!"); 
+    clearInterval(myInterval); 
+}, 2000);
+
+const startCountDown = (duration) =>{
+    let seconds = duration; 
+    const time = document.getElementById("time"); 
+    //time.innerText = seconds;
+   const intervalID = setInterval(() => {
+        console.log("Seconds:", seconds); 
+       // time.innerText = seconds;
+        seconds-=1; 
+        if (seconds < 0) {
+            clearInterval(intervalID); 
+           // time.innerText = "Time is Up!!";
+        }
+    }, 1000);
+}; 
+
+//startCountDown(0); 
+
+//Clear Timeout Example: 
+//const cancelButton = document.getElementById("cancel"); 
+
+// const timeoutID = setTimeout(() => {
+//     window.location.href = "https://www.google.com/"; 
+// }, 5000);
+
+// cancelButton.addEventListener("click", ()=>{
+//     clearTimeout(timeoutID); 
+//     console.log("Stopped the redirect")
+// }); 
+
+//Debouncing Example when conducting a live search by hitting a API: 
+const queryAPI = () =>{
+    console.log("Search the API"); 
+    console.log("Making an API Request"); 
+}; 
+
+const searchInput = document.getElementById("search"); 
+
+//First we clear the debounce, then after 1 second we hit the api with our search results: 
+let debounceTimeout; 
+searchInput.addEventListener("input", ()=>{
+    //Instead of hitting the API everytime, we can put this into a setTimeOut function: 
+    clearTimeout(debounceTimeout); 
+    debounceTimeout = setTimeout(() => {
+        queryAPI(); 
+    }, 1000);
+    
+})
---
+++ Ch.10 Timers/notes.md
@@ -0,0 +1,27 @@
+# Ch.10 Timers! Built in Browser APIs! 
+
+- setTimeout 
+    - Executes a function after waiting a specified number of milliseconds
+    - Built in function that allows us to excute code after a delay!
+    - Takes in a function, then runs it after a specific amount of time!
+    - One use case for this would be to set up notifications based off of a timer. 
+
+- setInterval
+    - Executes a function repeatedly, with a fixed time delay between each call. 
+    - Going to run a function repeatedly every second or entered duration
+
+- clearInterval
+    - Clears an interval set with setInterval, stopping the function from executing repeatedly. 
+    - We must save our interval or timeout to a ID variable then pass it through the clearInterval function! 
+    - Basically we just pass this a interval ID which will stop the interval function from running. 
+
+- clearTimeout
+    - Clears a timeout set with setTimeout, preventing the function from executing. 
+    - Works the same way as clearTimeout(myTimeout); 
+    - Telling JS to run something after a specific amount of seconds, then we will cancel it. 
+
+- Debouncing
+    - Adding a pause before the next function is ran via Debouncing, really useful for making a bunch of API calls. 
+    - We are essentially putting a pause while using our setTimeout
+    - Super important for FE development
+    - Soon as the event is triggered we will clear the timeout and then run the code again
