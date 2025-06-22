0b2bbfda868046415d671f1a5b645f525b9655ab
almondp040 <almondpaschal@gmail.com>
2025-06-22 16:05:53 -0400

    More Practice

--- Performace API and Web Audio/app.js
+++ Performace API and Web Audio/app.js
@@ -28,3 +28,97 @@ const measure = performance.measure("Native Sort Time", "Native Sort Start", "Na
 const nativeDuration = performance.getEntriesByName("Native Sort Time")[0].duration
 //See our values: 
 console.log( `Native took ${nativeDuration}`)
+
+//performance.getEntries grabs the performance metrics for everything! 
+console.log(performance.getEntries())
+
+//this allows you to pick out the stuff specifically inside of the getEntries array based off the name
+//Performance Entries: 
+const entries = ()=>{
+
+    let results = []; 
+    window.addEventListener("load", ()=>{
+    performance.getEntriesByType("resource")
+        .filter(entry => entry.initiatorType === "img")
+        .map((entry)=>{
+                 const duration = entry.duration; 
+                 const entryName = entry.name; 
+                 const script = `Here is our Duration based on performance: ${duration}ms to load, and here is our Entry Type: ${entryName}`; 
+                 results.push(script); 
+                 console.log(script); 
+                 return results; 
+    
+}); 
+
+
+}); 
+
+};  
+
+
+entries(); 
+
+//Web Audio API: Good for a prank lol: 
+// const audioContext = new AudioContext(); 
+// const oscillator = audioContext.createOscillator(); 
+// //Making our audio from scratch!
+// console.log(oscillator); 
+// //Say what type of sound wave you want: 
+// oscillator.type = "square"; 
+
+// //Apply this wave to the audio destination
+// oscillator.connect(audioContext.destination); 
+
+// //Play Sound: Really load tho
+// //oscillator.start()
+
+
+//Example: 
+const sliderFunctionality = () =>{
+    const context = new AudioContext(); 
+    const slider = document.querySelector("#freqSlider"); 
+    const playButton = document.querySelector("#playBtn"); 
+    const stopButton = document.querySelector("#stopBtn"); 
+    const span = document.querySelector("#freqLabel"); 
+
+   //Set up a null variable for use and reassignment: 
+    let oscillator = null; 
+
+
+    //on click play sound:
+    playButton.addEventListener("click", ()=>{
+
+        //Change oscillator into what is supposed to be: 
+    oscillator = context.createOscillator(); 
+    oscillator.type = "triangle"; 
+    oscillator.frequency.value = slider.value; 
+    oscillator.connect(context.destination); 
+    oscillator.start(); 
+        
+    })
+
+    //on click stop sound
+    stopButton.addEventListener("click", ()=>{
+        if (oscillator) {
+            oscillator.stop(); 
+            oscillator = null; 
+        }
+        
+    })
+
+
+    //Slider Funtionality: 
+    slider.addEventListener("input", (event)=>{
+        const frequency = event.target.value; 
+        span.textContent = `Frequency is ${frequency}hz`
+        if (oscillator) {
+            oscillator.frequency.value = frequency
+        }
+    }); 
+
+    
+   
+   
+}; 
+
+sliderFunctionality(); 
--- Performace API and Web Audio/notes.md
+++ Performace API and Web Audio/notes.md
@@ -4,4 +4,15 @@
     - Gives us performance timing data for pages, functions etc. 
     - Typically used to report performance data back to the devs. 
     - uses performance.mark and performance.measure to measure the results of our performance marks
-    - 
+    
+- Measuring Resource Load Times with Performance API
+    - using performance.getEntries we can get access to the performance data array
+    - getEntries looks at all components and grabs the performance of everything! 
+
+- Web Audio API
+    - Allows for the processing and synthesizing of autio in web applications
+    - By using AudioContext() we can make use of the Audio Context class and its methods!
+    - Alot of people also use a Library as well for this
+
+- Web Audio Use Cases:
+    - Instead of loading a audio file we can make one from scratch so that it's faster and easier for the browser, just not that easy to do!
