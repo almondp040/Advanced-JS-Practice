61fe0d21190688ca51f246d897ee8ab7698ee12e
almondp040 <almondpaschal@gmail.com>
2025-06-23 00:15:10 -0400

    Canvas API quick sesh

---
+++ Canvas API/app.js
@@ -0,0 +1,35 @@
+//Functionality of the Canvas API: 
+
+//Select our canvas: 
+const canvas = document.querySelector("#myCanvas"); 
+
+//Either 2D or 3D Drawing: 
+const context = canvas.getContext("2d"); 
+
+//How we can style: 
+// context.fillStyle= "rgba(255, 0, 0, 0.47)"
+// //Our 2 rectangles: 
+// context.fillRect(0, 0, 200, 80)
+// context.fillStyle= "rgba(17, 0, 255, 0.47)"
+// context.fillRect(100, 40, 100, 100)
+
+
+//How we can make different shapes using combinations of fillRect and clearRect: 
+// context.fillRect(50, 50, 200, 200); 
+// context.clearRect(50, 50, 100, 100); 
+
+//Can also so stroke styles
+// context.strokeStyle = "purple"; 
+// context.lineWidth = 5; 
+// context.strokeRect(50, 50, 100, 100); 
+
+//How to draw a basic line: 
+context.beginPath(); 
+context.moveTo(20, 100); 
+context.lineTo(100, 200); 
+context.lineTo(200, 100); 
+context.lineTo(-700, -800); 
+context.lineTo(220, -20); 
+context.fill(); 
+
+//To start a new path just use beginPath to start: 
---
+++ Canvas API/notes.md
@@ -0,0 +1,9 @@
+# Canvas API
+- Intro into Canvas API
+        - Enables drawing graphics via JS and the HTML <canvas> element
+        - Basically a way of drawing graphics, erase, 3D graphics and more!
+        - Adding cool stuff via drawing to webpages! 
+        - Also use CODEPEN for style examples or MDN Recommended Libraries!!
+
+- Canvas Basics
+    - See Canvas API File!
