510799e3dcf6331f6cc55221209791fd102f6272
almondp040 <almondpaschal@gmail.com>
2025-07-07 07:43:53 -0400

    Solid OOP Section Done

--- SOLIDOOP/app.js
+++ SOLIDOOP/app.js
@@ -68,3 +68,102 @@ console.log(AreaCalculator.calculate(circle));
 AreaCalculator.calculate(square); 
 
 //Liskov Substitution Principle
+class Bird {
+fly(){
+    console.log("This bird can fly!"); 
+}
+}
+
+class Duck extends Bird {
+fly(){
+    console.log("Ducks can fly!"); 
+}
+}
+
+//Then we can make another class with different birds using the same fly method!
+class Eagle extends Bird {
+    fly(){
+        console.log("Eagles can fly!"); 
+    }
+}
+
+function makeBirdFly(bird) {
+    bird.fly(); 
+}
+
+const duck = new Duck(); 
+
+makeBirdFly(duck); 
+
+
+//Interface Segregation Principle (ISP)
+class Worker {
+    work(){
+        console.log("Working on work!")
+    }
+
+    eat(){
+        console.log("Eating some food!")
+    }
+
+    sleep(){
+        console.log("Sleeping!")
+    }
+}
+
+class Workable {
+        work(){
+        console.log("Working on work!") 
+        }
+}
+
+//This function will depend on the work method in the worker class, in this case we would not be supporting ISP
+//To adhere to ISP we would have a class for each method instead of having everything tied to Worker
+function manageWork(workable) {
+    workable.work()
+}
+
+
+//Dependency Inversion Principle (DIP)
+//Basically make 1 main class, then the following classes will depend on the main class, have classes be related to each other
+
+class AbstractAuthMethod {
+    authenticate(credientials){
+        throw new Error("This method should be overridden!");
+    }
+}
+
+class UsernamePasswordAuth extends AbstractAuthMethod {
+    authenticate({username, password}){
+        //Logic to authenticate using username and password
+    }
+}
+
+class EmailAuth extends AbstractAuthMethod {
+    constructor({email, token}) {
+        //Logic to auth using email and token sent to the email
+    }
+}
+
+//Then to use it: 
+class User {
+    constructor(authMethod) {
+        if (!(authMethod instanceof AbstractAuthMethod)) {
+            throw new Error("Invalid Auth Method!");
+        }
+
+        this.authMethod = authMethod
+    }
+
+    //Login Method: 
+    login(credientials){
+        return this.authMethod.authenticate(credientials); 
+    }
+
+}
+
+//Anytime we wanna have a new auth method, we will not need to change the User class, we can simply create a new method that extends the Abstract Auth method
+
+
+//Law of Demeter
+//Write classes or objects that only interact with the other immediate object
--- SOLIDOOP/notes.md
+++ SOLIDOOP/notes.md
@@ -14,4 +14,24 @@
 
 - Liskov Substitution Principle
     - Objects of a superclass should be able to be replaced with objects of a subclass without affecting the correctness of the program. 
+    - Making sure a subclass can stand in for the parent class without making any problems
+    - Basically making subclasses for the main class with the same methods 
+        - All subclasses should be able to use all of the methods in the parent class
+
+- Interface Segregation Principle (ISP)
+    - A class should not be forced to implement interfaces it does not use
+    - JS doesn't have interfaces in the way Java or Typescript does, but the principle can still be applied conceptually using duck typing or optional methods 
+    - Classes should NOT be forced to depend on interfaces they will NOT use!
+    
+- Dependency Inversion Principle (DIP)
+    - High Level Application Logic (modules) should not depend directly on low level modules such as specific tools or libraries
+    - Basically build your main application logic yourself 
+    - Both should depend on abstractions (interfaces or general ideas)
+    - Highlevel code should not depend on low level code!
+
+- Law of Demeter
+    - The main idea is to ensure that our objects don't reveal too much about their structure or their collaborators structures
+    - For a given object, it should only talk to its friends
+    - We want to stay away from chaining multiple layers of objects
+    - AKA KEEP CLASSES AS SIMPLE AS POSSIBLE!
 
