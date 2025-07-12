8d46a91be87bba8c2573cbe88aca69a983424093
almondp040 <almondpaschal@gmail.com>
2025-07-12 13:09:39 -0400

    More Design Patterns!

--- DesignPatterns/app.js
+++ DesignPatterns/app.js
@@ -65,3 +65,135 @@ class DatabaseConnection {
 
     //Can enter other DB Methods if needed!
 }
+
+//Observation Pattern: 
+//Imagine the subject class as a button and we apply methods and functions to it
+//Subjects is an array then our methods will determine what happens to the array
+class Subject {
+    constructor(parameters) {
+        this.observers = [];
+    }
+    subcribe(fn){
+        this.observers.push(fn); 
+    }
+
+    unsubcribe(fn){
+      this.observers = this.observers.filter((observe)=> observe !== fn); 
+    }
+
+    notify(data){
+        this.observers.forEach(fn => fn(data)); 
+    }
+}
+
+const observer1 = (data) => console.log("Observer 1", data);
+const observer2 = (data) => console.log("Observer 2", data); 
+const observer3 = (data) => console.log("Observer 3", data);  
+
+const nums = [1,2,3,4,5]; 
+observer1(nums); 
+observer2(nums); 
+observer3(nums); 
+
+const subject = new Subject(); 
+subject.subcribe(observer1); 
+subject.subcribe(observer2); 
+subject.subcribe(observer3); 
+
+
+subject.notify(nums); 
+
+//Subcriber Example 
+//A great use case we should write out our classes and what they do before we code!!
+class Subscriber {
+    constructor(parameters) {
+        this.subscriber = []
+    }
+
+subcribe(subcribers){
+    this.subscriber.push(subcribers); 
+}
+
+unsubcribe(subcribers){
+    this.subscriber = this.subscriber.filter((sub)=> sub !== subcribers)
+}
+
+publish(post){
+    this.subscriber.forEach((subcribers)=> subcribers.notify(post)); 
+}
+}
+
+class ActualSub {
+    constructor(name) {
+        this.name = name; 
+    }
+
+    notify(post){
+        const script = `${this.name} received a notification: New Post Published: ${post}`; 
+        console.log(script)
+    }
+}
+
+const Allen = new ActualSub("Allen"); 
+const Barry = new ActualSub("Barry"); 
+
+const blog = new Subscriber(); 
+
+const allenSubs = blog.subcribe(Allen); 
+
+console.log(Allen.notify("New Post from Allen")); 
+
+//Registry Pattern: Like this pattern the most!
+//Here's our chicken data in the form of a class
+//Creates a shared centralized place that contains all methods tied to the object
+class Chicken {
+    constructor(id, name, age, breed) {
+        this.id = id, 
+        this.name = name, 
+        this.age = age, 
+        this.breed = breed
+    }
+    //Chicken Methods: 
+}
+
+//Chicken class represents our data!!
+class ChickenRegistry {
+    #chickens = new Map(); 
+
+    //Passes in a chicken with the items from the Chicken Class
+    addChicken(chicken){
+        if (!chicken.id) {
+            throw new Error("Chicken must have a ID!");
+        }
+
+        this.#chickens.set(chicken.id, chicken); 
+    }
+
+    getChicken(id){
+        console.log(this.#chickens.get(id))
+        return this.#chickens.get(id); 
+    }
+
+    removeChicken(id){
+        this.#chickens.delete(id); 
+    }
+
+    getAllChickens(){
+        return [...this.#chickens.values()]; 
+    }
+
+}
+
+const chicken1 = new Chicken(1, "Almond", 5, "Super Chicken"); 
+
+const chickenRegistry = new ChickenRegistry(); 
+
+const addChicken1 = chickenRegistry.addChicken(chicken1); 
+chickenRegistry.getChicken(1); 
+
+
+
+
+
+
+
--- DesignPatterns/notes.md
+++ DesignPatterns/notes.md
@@ -10,3 +10,16 @@
     - Redux store in React applications acts as a Singleton, ensuring a singular store instance
     - Node.js module system acts like a signleton as well in regards to working with modules
     - Describe the shape or function of the object, check to make sure only 1 has been created then return it
+
+- Observation Pattern
+    - The Observation Pattern enables a subscription model where objects (observers) listen to events and get notified when events occur
+    - Real World Examples include addEventListeners in the DOM that are built upon the Observer Pattern 
+    - Once the state of the subject is changed, the observers are notified
+    - Vue.js uses a reactice data model where component re-renders are triggered by data changes, which is an application of the Observer Pattern. 
+    - RxJS is a library that provides utilities for working with Observables
+
+- Registry Pattern
+    - The registry pattern is a design pattern used to store and retrieve instances of objects
+    - Idea is that we have a centralized manager or a database of our objects and from there we can use the data how we want 
+    - It acts like a central place to manage the objects, and it is particularly useful when you want to access the instances from different parts of your application without needing to pass them around as parameters.
+    - Major risk is having a single point of failure
